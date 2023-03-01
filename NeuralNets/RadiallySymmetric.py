e4652ade4c3e80d5945480abefd708d58658f36d
AlinaRinn <nik17042001@rambler.ru>
2023-03-02 02:57:22 +0300

    I try to iterate, but catch some timegate

--- NeuralNets/RadiallySymmetric.py
+++ NeuralNets/RadiallySymmetric.py
@@ -1,15 +1,13 @@
-from cmath import nan
 import numpy as np
 from math import sqrt
 from Common import E
 
-def Gauss(InputArray, vecC, R):
+def Gauss(InputArray, vecC, R, Eras):
     Alfa = 1 / (2*R**2)
 
     X = []
     Y = []
 
-    H = []
     h = []
 
     W = []
@@ -21,20 +19,25 @@ def Gauss(InputArray, vecC, R):
     W = np.array(W, float)
     print(X)
     print(vecC)
+    iterator = 0
+    while(iterator < Eras):
+        H = []
+        for i in range(len(vecC)):
+            for j in range(len(X)):
+                h.append(E**(-Alfa*sqrt((X[j]-vecC[i])**2)**2))
+            H.append(h.copy()) # Oni, syka, iz roditel'skogo massiva udalyautcha bez copirovania!! Piton govno
+            h.clear()
 
-    for i in range(len(vecC)):
-        for j in range(len(X)):
-            h.append(E**(-Alfa*sqrt((X[j]-vecC[i])**2)**2))
-        H.append(h.copy()) # Oni, syka, iz roditel'skogo massiva udalyautcha bez copirovania!! Piton govno
-        h.clear()
+        H = np.array(H, float)
+        H = H.T
+        print(H)
+        HT = H.T
 
-    H = np.array(H, float)
-    H = H.T
-    print(H)
-    HT = H.T
+        W = np.dot(np.dot(np.linalg.inv(np.dot(HT, H)), HT), Y.T)
+        print(W)
+        vecC = W
+        iterator += 1
 
-    W = np.dot(np.dot(np.linalg.inv(np.dot(HT, H)), HT), Y.T)
-    print(W)
 
 
 def main():
@@ -48,5 +51,5 @@ def main():
                   [1.5, 1.17], 
                   [2.0, 0.20]]
     vecC = [-2.0, -1.0, 0.0, 1.0, 2.0] # Vector of centers
-    Gauss(InputArray, vecC, 1.5)
+    Gauss(InputArray, vecC, 1.5, 10)
 main()
