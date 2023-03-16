19c5ee658fe07ffe38d8194d322f8097c47bbe65
AlinaRinn <nik17042001@rambler.ru>
2023-03-16 21:00:32 +0300

    fix

--- NeuralNets/AutoassociativeMemory.py
+++ NeuralNets/AutoassociativeMemory.py
@@ -1,35 +1,41 @@
 import numpy as np
 from Common import SingleJump
 
-def Hopfield(X, W, Practice, T):
-    W = np.array(W, int)
-    X = np.array(X, int)
-    Y0 = np.array(Practice[0], int)
-    Y1 = []
-    S = []
-    W = np.dot(X.T, X)
+def Hopfield(X, Xprac):
+    W = np.array([], int)
+    X = np.array(X, int) # 1.1
+    W = np.dot(X.T, X) # 1.2
     for i in range(len(W)):
         for j in range(len(W[0])):
             if(i == j):
                 W[i][j] = 0
-    print(W)
-    print(Y0)
-    n = 1.0
-    while(n > 0):
-        for i in range(len(W)):  # 2.2
-            tmp = 0
-            for j in range(len(W[0])):
-                tmp += W[i][j]*Y0[j]
-            S.append(tmp)
-        
-        print(S)
-        Y1.tolist()
-        for j in range(len(Y0)):
-            Y1.append(SingleJump(S[j], T))
-        Y1 = np.array(Y1, int)
-        print(Y1)   
-        n = np.sqrt(sum((Y1 - Y0)**2))
-        print(n)
+    print("W:\n", W)
+
+    for prac in range(len(Xprac)):
+        Y = [Xprac[prac]] # 2.1
+        print("Y0:\n", Y)                   
+        n = 1.0
+        counter = 0
+        while(n > 0.0):
+            temp = [] 
+            S = []
+            for i in range(len(W)):  # 2.2
+                tmp = 0
+                for j in range(len(W[0])):
+                    tmp += W[i][j]*Y[counter][j]
+                S.append(tmp)
+            print("S:\n", S)
+            Y = list(Y)
+            temp = [] 
+            for j in range(len(Y[0])): # 2.3
+                temp.append(SingleJump(S[j], 0))
+            Y.append(temp)
+            Y = np.array(Y, int)
+            print("Y1:\n", Y[counter+1])   
+            print("Y1 - Y0:\n", Y[counter] - Y[counter+1])
+            n = sum((Y[counter] - Y[counter+1])**2) # 2.4
+            print("n:\n", n)
+            counter += 1
 
 
 
@@ -38,6 +44,5 @@ def main():
                   [1, 1, 1, 1, -1, 1, 1, -1, -1, 1, 1, -1, 1, 1, 1, 1]] # 2 
     Practice =   [[1, 1, 1, -1, 1, -1, 1, 1, 1, -1, -1, 1, 1, -1, -1, 1], # 1 mod
                   [1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, 1, -1, 1, 1]] # 2 mod
-    W = []
-    Hopfield(InputArray, W, Practice, 0)
+    Hopfield(InputArray, Practice)
 main()
