bf30201b2964e6a5aabb7c5efb37a95fded7099a
Nikita Lu <nik17042001@outlook.com>
2023-03-27 16:17:40 +0300

    test

---
+++ Hamming/Hamming.py
@@ -0,0 +1,44 @@
+import Common
+
+def Hamming(X, Xn): 
+    print(X)
+    W = list(Common.multiplier(X, 0.5))
+    print(W)
+    T = len(W[0]) / 2
+    eps = 1 / len(X)
+    E = [[round(-eps, 1) for j in range(len(X))] for i in range(len(X))]
+    E = Common.diagonalNullifier(E, 1)
+    print(E)
+    Emax = 0.1
+    for e in range(len(Xn)):
+        print("\nImage", e)
+        S1 = Common.summator(Common.multiply(W, Common.transpose([Xn[e]])), T)
+        Y1 = list([[Common.LinearThreshold(S1[i][0], T) for i in range(len(S1))]])
+        print(S1, "\n\n", Y1)
+        Y2 = Y1
+        Yprev = Y1
+        check = 1
+        counter = 0
+        while(check >= Emax):
+            print("\nIter ", counter, "\n----------------------------------------")
+            Yprev = Y2
+            S2 = Common.multiply(E, Common.transpose(Y2))
+            Y2 = list([[Common.LinearThreshold(S2[i][0], T) for i in range(len(S2))]])
+            check = sum(Common.powList(Common.difference2dim(Y2, Yprev)))
+            counter += 1
+            print("S2: ", S2, "\nY2", Y2, "\n", "Check: ", check)
+        
+        
+def main(): 
+    inputArray = [[1, -1, 1, -1, 1, -1, 1, -1, 1],
+                  [-1, 1, -1, 1, 1, 1, -1, 1, -1],
+                  [1, 1, 1, 1, -1, 1, 1, 1, 1]]
+    testArray = [[1, -1, -1, -1, 1, -1, 1, -1, 1], 
+                 [1, 1, 1, -1, -1, -1, 1, 1, 1]]
+    outputArray = list()
+    #inputArray.append(binArrayParser(imageRecognizer('mono128x128/img3mono.bmp')))
+
+   
+
+    Hamming(inputArray, testArray)
+main()
